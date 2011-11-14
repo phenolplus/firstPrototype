68488e6f13d7df3cfb1161d4a5c6ff5348df2f04
Vincent <phenolplus@hotmail.com>
2011-11-14 13:56:09 +0800

    first upload

---
+++ src/prototype/first/test/FirstPrototypeActivity.java
@@ -0,0 +1,14 @@
+package prototype.first.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstPrototypeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+    }
+}
