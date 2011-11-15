c7e8c25654d03d3e34dfccf8c7dbeb6f05c7b90d
Vincent <phenolplus@hotmail.com>
2011-11-15 12:55:46 +0800

    North added

--- src/prototype/first/test/DrawingSurface.java
+++ src/prototype/first/test/DrawingSurface.java
@@ -1,34 +1,41 @@
 package prototype.first.test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.hardware.Camera;
+import android.util.Log;
 import android.view.SurfaceHolder;
 
 public class DrawingSurface extends android.view.SurfaceView implements SurfaceHolder.Callback{
 
 	/** Members */
-	private float[] north = {0, 0, 90};
-	private float[] current = new float[3];
+	private Camera camera;
+	private SurfaceHolder holder;
 	
+	private final float[] north = {0, 0, 90};
+	private ArrayList<float[]> targetList = new ArrayList<float[]>();
+	private float[] current = new float[3];
+	private final float distance = 50;
 	
 	public DrawingSurface(Context context) {
 		super(context);
+		current = north;
 		this.setWillNotDraw(false);
-		
+		holder = this.getHolder();
+		holder.addCallback(this);
+		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
 	public void onDraw(Canvas canvas){
-		Paint paint = new Paint();
-		paint.setColor(Color.BLUE);
-		paint.setAlpha(100);
-		float h,w;
-		h = canvas.getHeight();
-		w = canvas.getWidth();
-		canvas.drawCircle(w/2, h/2, 30, paint);
+		canvas = aquireNorth(canvas);
+		canvas = showTargets(canvas);
 	}
 
 	@Override
@@ -41,23 +48,82 @@ public class DrawingSurface extends android.view.SurfaceView implements SurfaceH
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
+		camera = Camera.open();
+		try {
+			camera.setPreviewDisplay(holder);
+		} catch (IOException exception) {
+			Log.e("IO Exception catch", "Did come into exception");
+			camera.release();
+			camera = null;
+		}
+		Camera.Parameters para = camera.getParameters();
+		para.setPreviewSize(this.getWidth()/2, this.getHeight()/2);
+		camera.setParameters(para);
+		camera.startPreview();
 		
 	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
-		
+		camera.stopPreview();
+		camera.release();
+		camera = null;
 	}
 	
-	/** Utilities */
-	public void setTarget(float[] direction){
-		
-	}
-	
+	/** Importing data */
 	public void setCurrent(float[] direction){
 		current = direction;
 	}
 	
+	public void importTargetList(){
+		
+	}
+	
+	/** Utilities */
+	private Canvas aquireNorth(Canvas canvas){
+		float [] point = new float[2];
+		// point = [x,y] = [polar,roll]
+		
+		point[1] = north[2] - current[2];
+		if(current[0]>180){
+			point[0] = north[0] - (current[0] - 360);
+			
+		} else {
+			point[0] = north[0] - current[0];
+			
+		}
+		point[0] = point[0]*distance;
+		point[1] = point[1]*distance;
+		
+		Paint paint = new Paint();
+		paint.setColor(Color.BLUE);
+		paint.setAlpha(127);
+		
+		float x,y;
+		y = canvas.getHeight()/2 + point[1];
+		x = canvas.getWidth()/2 + point[0];
+		
+		canvas.drawCircle(x, y, 30, paint);
+		paint.setAlpha(5);
+		paint.setColor(Color.WHITE);
+		canvas.drawText("N", x, y, paint);
+		
+		return canvas;
+	}
+	
+	private Canvas showTargets(Canvas canvas){
+		Paint paint = new Paint();
+		paint.setAlpha(127);
+		paint.setColor(Color.RED);
+		
+		float x,y;
+		for(int i=0;i<targetList.size();i++){
+			x = this.getWidth()/2 + targetList.get(i)[0];
+			y = this.getHeight()/2 + targetList.get(i)[1];
+			canvas.drawCircle(x, y, 30, paint);
+		}
+		return canvas;
+	}
 
 }
